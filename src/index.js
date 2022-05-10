2cc2e10ea821aa1e61c60fceb256c7becd7ac007
한태경 <roenh@statnco.com>
2022-05-10 14:26:58 +0900

    sdf

--- src/index.js
+++ src/index.js
@@ -58,12 +58,12 @@ const joinUrl = () => {
     }, 1000);
 
     // // location.replace(launchAppUrl);
-    window.open(launchAppUrl);
+    // window.open(launchAppUrl);
     // window.open("https://naver.com");
     // if (isIOS) {
     //     prevType = "isIosFocus";
     // }
-    // window.location = launchAppUrl;
+    window.location = launchAppUrl;
 };
 joinBtn.addEventListener("click", joinUrl);
 aBtn1.addEventListener("click", () => {
